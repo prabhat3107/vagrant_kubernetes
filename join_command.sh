99a607d1f707e21af30d23baae9a00b2781b06da
prabhat3107 <prabhat3107@gmail.com>
2025-11-09 18:05:45 -0500

    Added code to use containerd as Kubernetes CRI

--- join_command.sh
+++ join_command.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.10:6443 --token 6z6wvl.9992zscy3sz4lqkn --discovery-token-ca-cert-hash sha256:0f72465337c2e913d3bf256880fa5258af6554d087e08c414261a1468f51d6e0 
+kubeadm join 192.168.56.10:6443 --token j87wka.u74jh4p04ucboczu --discovery-token-ca-cert-hash sha256:f4f5771a633472cbea89fde8a28371a7fc1d68fc7e255dab13479f8056727245 
--- scripts/master.sh
+++ scripts/master.sh
@@ -2,14 +2,15 @@
 # Initialize Kubernetes master
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.56.10
 
-while [ ! -f /etc/kubernetes/admin.conf ]; do
-  sleep 5
-done
-
 # Configure kubectl for vagrant user
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+mkdir -p /home/vagrant/.kube
+sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+sudo chown vagrant:vagrant /home/vagrant/.kube/config
 
 # Install a Pod Network Add-on (e.g., Flannel)
 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
